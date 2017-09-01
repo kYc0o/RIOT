0d97e6e3f6fa787caef9f48395ba8b47a34ae913
Kaspar Schleiser <kaspar@schleiser.de>
2017-09-01 10:53:08 +0200

    squash: add event_get()

--- sys/event/event.c
+++ sys/event/event.c
@@ -36,6 +36,17 @@ void event_cancel(event_queue_t *queue, event_t *event)
     irq_restore(state);
 }
 
+event_t *event_get(event_queue_t *queue)
+{
+    unsigned state = irq_disable();
+    event_t *result = (event_t *) clist_lpop(&queue->event_list);
+    irq_restore(state);
+    if (result) {
+        result->list_node.next = NULL;
+    }
+    return result;
+}
+
 event_t *event_wait(event_queue_t *queue)
 {
     thread_flags_wait_any(THREAD_FLAG_EVENT);
